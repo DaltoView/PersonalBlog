5ce40dff8db1d081a50ba53e21c07c2152a42073
Dima Semenko <semenkodima97@gmail.com>
2019-04-17 19:43:18 +0300

    Add CORS

--- WebApi/App_Start/WebApiConfig.cs
+++ WebApi/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Web.Http.ExceptionHandling;
 using WebApi.Filters;
 
@@ -11,6 +12,7 @@ namespace WebApi
             // Web API configuration and services
             config.Services.Replace(typeof(IExceptionHandler), new GlobalExceptionHandler());
             config.Services.Add(typeof(IExceptionLogger), new BlogExceptionLogger());
+            //config.EnableCors(new EnableCorsAttribute("http://localhost:4200", headers: "*", methods: "*"));
             // Web API routes
             config.MapHttpAttributeRoutes();
 
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.Owin;
 using Owin;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 [assembly: OwinStartup(typeof(WebApi.Startup))]
 
@@ -13,6 +9,7 @@ namespace WebApi
     {
         public void Configuration(IAppBuilder app)
         {
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
             ConfigureAuth(app);
         }
     }
